f640244f2f0ffb9af9279a89d10481ba5e54ba30
LeAnne B <branchleannelmb@gmail.com>
2024-05-17 15:48:15 -0400

    adds bottom yellow line

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ int main () {
         BeginDrawing();
         ClearBackground(grey);
         DrawRectangleRoundedLines({10, 10, 780, 780}, 0.18f, 20, 2, yellow);
+        DrawLineEx({25, 730}, {775, 730}, 3, yellow);
         game.Draw();
         EndDrawing();
     }
